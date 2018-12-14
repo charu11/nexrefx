61026900f94f200df26d823c6a08007c03828253
Rakshitha Muranga Rodrigo <pathfinder.rover11b@gmail.com>
2018-12-15 04:23:23 +0530

    + test 3

--- controllers/otp.js
+++ controllers/otp.js
@@ -51,36 +51,36 @@ router.use(function (req, res, next) {
 });
 
 
-let isConnected = false
-session.on('connect', () => {
-  isConnected = true;
-  console.log("session started")
-  session.bind_transceiver({
-      system_id: 'NEXGEN',
-      password: 'Nexgen@1'
-      // interface_version: 1,
-      // system_type: '380666000600',
-      // address_range: '+380666000600',
-      // addr_ton: 1,
-      // addr_npi: 1,
-  }, (pdu) => {
-    if (pdu.command_status == 0) {
-        console.log('Successfully bound')
-    }
-    });
-  })
-});
-session.on('close', () => {
-  console.log('smpp is now disconnected')
-
-  if (isConnected) {
-    session.connect();    //reconnect again
-  }
-});
-session.on('error', error => {
-  console.log('smpp error', error)
-  isConnected = false;
-});
+// let isConnected = false
+// session.on('connect', () => {
+//   isConnected = true;
+//   console.log("session started")
+//   session.bind_transceiver({
+//       system_id: 'NEXGEN',
+//       password: 'Nexgen@1'
+//       // interface_version: 1,
+//       // system_type: '380666000600',
+//       // address_range: '+380666000600',
+//       // addr_ton: 1,
+//       // addr_npi: 1,
+//   }, (pdu) => {
+//     if (pdu.command_status == 0) {
+//         console.log('Successfully bound')
+//     }
+//     });
+//   })
+// });
+// session.on('close', () => {
+//   console.log('smpp is now disconnected')
+//
+//   if (isConnected) {
+//     session.connect();    //reconnect again
+//   }
+// });
+// session.on('error', error => {
+//   console.log('smpp error', error)
+//   isConnected = false;
+// });
 
 
 // function sendSMS(from, to, text){
