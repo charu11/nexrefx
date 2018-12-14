38ad67eb482001f3915a38ad28a1f097cdf3e2df
Rakshitha Muranga Rodrigo <pathfinder.rover11b@gmail.com>
2018-12-15 04:04:31 +0530

    +smpp

--- controllers/otp.js
+++ controllers/otp.js
@@ -22,7 +22,8 @@ var fs = require('fs');
 var path = require('path');
 var jsonwebtoken = require('jsonwebtoken');
 var request=require('request');
-var smsGateway = require('sms-gateway-nodejs')('NEXGEN', 'Nexgen@1')
+const smpp = require('smpp');
+const session = new smpp.Session({host: '119.235.5.234', port: 5019});
 
 //var User = mongoose.model('User');
 
@@ -50,16 +51,63 @@ router.use(function (req, res, next) {
 });
 
 
+let isConnected = false
+session.on('connect', () => {
+  isConnected = true;
+
+  session.bind_transceiver({
+      system_id: 'NEXGEN',
+      password: 'Nexgen@1'
+      // interface_version: 1,
+      // system_type: '380666000600',
+      // address_range: '+380666000600',
+      // addr_ton: 1,
+      // addr_npi: 1,
+  }, (pdu) => {
+    if (pdu.command_status == 0) {
+        console.log('Successfully bound')
+    }
+    });
+  })
+})
+session.on('close', () => {
+  console.log('smpp is now disconnected')
+
+  if (isConnected) {
+    session.connect();    //reconnect again
+  }
+})
+session.on('error', error => {
+  console.log('smpp error', error)
+  isConnected = false;
+});
+
+
+function sendSMS(from, to, text) {
+   from += `+${from}`
+// this is very important so make sure you have included + sign before ISD code to send sms
+
+  to += `+${to}`
+
+  session.submit_sm({
+      source_addr:      from,
+      destination_addr: to,
+      short_message:    text
+  }, function(pdu) {
+      if (pdu.command_status == 0) {
+          // Message successfully sent
+          console.log(pdu.message_id);
+      }
+  });
+}
+
 exports.otp = function(req, res){
-  console.log("###### user ######");
-  smsGateway.device.listOfDevices(2)
-  .then((response) => {
+  console.log("###### OTP ######");
   // do something with respons
-  res.json({status: 'user auth success !', response : response});
-})
-.catch((error) => {
-  // handle error
-})
+  sendSMS("94115936540","94711358399","TestOTP")
+  
+  // res.json({status: 'user auth success !', response : response});
+
 
 };
 
