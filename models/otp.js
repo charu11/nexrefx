164e645732e2d333f26ff84205a9bd9786aa6118
Rakshitha Muranga Rodrigo <pathfinder.rover11b@gmail.com>
2018-12-29 13:46:22 +0530

    + otp model
    "API" added

--- models/otp.js
+++ models/otp.js
@@ -10,7 +10,7 @@ var OtpSchema = Schema({
     content: { type: String, required: true, unique: false },
     recieverNumber: { type: String, required: true, unique: false },
     type: { type: String, required: true, unique: false, enum:['otp', 'bulk', 'custom'] },
-    userPlatform: { type: String, required: true, unique: false, enum:['iOS', 'android', 'web']},
+    userPlatform: { type: String, required: true, unique: false, enum:['iOS', 'android', 'web', 'API']},
     recordedTime:  { type: Date, required: true, unique: false, default: Date.now}
 });
 
